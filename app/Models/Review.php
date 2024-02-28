843c3d6b99deb5f3cf20c2ef86fbf68b5094fde6
Miguel Hoyos <mahoyosv@eafit.edu.co>
2024-02-28 09:48:26 -0500

    Creation of some migrations and models with primitive attributes

---
+++ app/Models/Review.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+   
+    /**
+     * PRODUCT ATTRIBUTES
+     * $this->attributes['id'] - int - contains the product primary key (id)
+     * $this->attributes['description'] - string - contains the description of the review
+     * $this->attributes['rating'] - int - contains the comment rating
+     */
+    protected $fillable = ['description', 'rating'];
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id): void
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->attributes['description'];
+    }
+
+    public function setDescription($description): void
+    {
+        $this->attributes['description'] = $description;
+    }
+
+    public function getRating(): int
+    {
+        return $this->attributes['rating'];
+    }
+
+    public function setRating($rating): void
+    {
+        $this->attributes['rating'] = $rating;
+    }
+}
---
+++ database/migrations/2024_02_28_143935_create_challenges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up() : void
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->id(); // Auto-incrementing ID
+            $table->string('name'); // Challenge name
+            $table->string('description'); // Challenge description
+            $table->boolean('checked'); // Flag to indicate if challenge is checked
+            $table->integer('reward_coins'); // Number of reward coins for completing the challenge
+            $table->integer('max_users'); // Maximum number of users allowed for the challenge
+            $table->integer('current_users'); // Current number of users participating in the challenge
+            $table->string('product_name'); // Name of the product associated with the challenge
+            $table->integer('product_quantity'); // Quantity of the product associated with the challenge
+            $table->timestamps(); // Timestamps for created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down() : void
+    {
+        Schema::dropIfExists('challenges'); // Drop the challenges table
+    }
+};
