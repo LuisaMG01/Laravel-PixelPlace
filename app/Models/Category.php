cfe2a227d2bbbc83bd2050bd6a56f1c762d13e96
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2024-02-29 11:17:04 -0500

    correction for challenge relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -40,10 +40,10 @@ class Category extends Model
     {
         return $this->hasMany(Product::class);
     }
-    
-    /**Change request */
+       
     public function challenges():HasMany
     {
         return $this->hasMany(Challenge::class);
     }
+     /**Change request */
 }
