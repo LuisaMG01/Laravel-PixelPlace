0ec983a8ad121db86edab466aea96cff7053d2b7
JuanM0412 <juangomezp1204@gmail.com>
2024-03-09 11:27:01 -0500

    Product model and formatter

--- database/migrations/2024_02_28_144624_create_challenge_users_table.php
+++ database/migrations/2024_02_28_144624_create_challenge_users_table.php
@@ -9,10 +9,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('challenge_users', function (Blueprint $table) {
-            $table->id(); 
-            $table->timestamps(); 
-            $table->integer('progress'); 
-            $table->boolean('checked'); 
+            $table->id();
+            $table->timestamps();
+            $table->integer('progress');
+            $table->boolean('checked');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('challenge_id');
@@ -22,6 +22,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('challenge_users'); 
+        Schema::dropIfExists('challenge_users');
     }
 };
