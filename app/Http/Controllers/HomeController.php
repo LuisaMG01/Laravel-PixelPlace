4304086bbebb673ece6ff3c99ac8bd6e295e9e89
JuanM0412 <juangomezp1204@gmail.com>
2024-05-18 15:45:59 -0500

    Add locale route and controller method for setting the language (lang)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class HomeController extends Controller
 {
@@ -17,5 +18,12 @@ class HomeController extends Controller
         ];
 
         return view('home')->with('viewData', $viewData);
+    }
+
+    public function locale(string $locale): RedirectResponse
+    {
+        session(['locale' => $locale]);
+
+        return redirect()->back()->withCookie('locale', $locale);
     }
 }
