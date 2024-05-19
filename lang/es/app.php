deaf2075660d4abd1191c153d45750828dbe7343
JuanM0412 <juangomezp1204@gmail.com>
2024-05-19 14:51:47 -0500

    Some missing translations were added

--- lang/es/app.php
+++ lang/es/app.php
@@ -2,31 +2,31 @@
 
 return [
 
-    // Mensajes de inicio
+    // Home messages
     'title_home' => '¡Únete a la Revolución Gamer en PixelPlace!',
-    'principal_banner_home_text' => 'En PixelPlace, no solo vendemos productos para gamers, ¡construimos una comunidad! Participa en emocionantes desafíos y demuestra tus habilidades. ¿Listo para el reto? ¡Completa nuestros desafíos y obtén un descuento en tu próxima compra!',
+    'principal_banner_home_text' => 'En PixelPlace, no solo vendemos productos gamer, ¡construimos una comunidad! Participa en desafíos emocionantes y demuestra tus habilidades. ¿Listo para el desafío? ¡Completa nuestros desafíos y asegura un descuento en tu próxima compra!',
     'see_products_home_button' => 'Ver Productos',
     'login_home_button' => 'Iniciar Sesión',
     'register_home_button' => 'Registrarse',
-    'title_second_banner_home' => '¿Por Qué Elegir PixelPlace?',
-    'content_second_banner_home' => 'Descubre por qué gamers de todo el mundo confían en PixelPlace para sus necesidades de gaming. Desde productos de la más alta calidad hasta un servicio al cliente inigualable, tenemos todo lo que necesitas para llevar tu experiencia de juego al siguiente nivel.',
+    'title_second_banner_home' => '¿Por qué elegir PixelPlace?',
+    'content_second_banner_home' => 'Descubre por qué los gamers de todo el mundo confían en PixelPlace para sus necesidades de gaming. Desde productos de alta calidad hasta un servicio al cliente inigualable, tenemos todo lo que necesitas para llevar tu experiencia de juego al siguiente nivel.',
     'subtitle_one_second_banner_home' => 'Productos de Alta Calidad',
-    'content_one_second_banner_home' => 'Explora nuestra selección curada de equipos de gaming de alta calidad, desde periféricos de última generación hasta hardware de alto rendimiento, todo diseñado para elevar tu experiencia de juego.',
+    'content_one_second_banner_home' => 'Explora nuestra selección curada de equipos de gaming de alta calidad, desde periféricos de última generación hasta hardware de alto rendimiento, todo diseñado para mejorar tu experiencia de juego.',
     'subtitle_two_second_banner_home' => 'Envío Rápido y Seguro',
-    'content_two_second_banner_home' => 'Disfruta de opciones de envío rápidas y seguras a nivel mundial. Con nuestra red logística eficiente, tu equipo de gaming llegará a tu puerta en poco tiempo, listo para alimentar tus aventuras de juego.',
+    'content_two_second_banner_home' => 'Disfruta de opciones de envío rápido y seguro en todo el mundo. Con nuestra eficiente red logística, tu equipo de gaming estará en tu puerta en poco tiempo, listo para impulsar tus aventuras de juego.',
     'subtitle_three_second_banner_home' => 'Soporte al Cliente 24/7',
-    'content_three_second_banner_home' => '¿Tienes preguntas o necesitas asistencia? Nuestro dedicado equipo de soporte al cliente está disponible 24/7 para brindarte asistencia experta y garantizar una experiencia de compra sin problemas.',
+    'content_three_second_banner_home' => '¿Tienes preguntas o necesitas asistencia? Nuestro dedicado equipo de soporte al cliente está disponible 24/7 para brindarte asistencia experta y asegurar una experiencia de compra sin problemas.',
     'title_second_banner_images_products_home' => '¡Ve nuestros productos más vendidos!',
 
-    // Mensajes de pie de página
+    // Footer messages
     'caption_footer' => '© 2024',
     'all_rights_footer' => 'Todos los Derechos Reservados.',
 
-    // Mensajes de paginación
+    // Pagination messages
     'showing_pagination' => 'Mostrando',
     'of_pagination' => 'de',
 
-    // Mensajes de índice de productos
+    // Index product messages
     'welcome_to_product_section_product_index' => '¡Bienvenido a la sección de productos!',
     'success_creation_review' => 'La reseña se ha creado con éxito',
     'success_product_index' => '¡Éxito!',
@@ -35,7 +35,7 @@ return [
     'category_product_index' => 'Categoría',
     'all_categories_product_index' => 'Todas las categorías',
     'price_product_index' => 'Precio',
-    'select_price_range_product_index' => 'Seleccionar rango de precios',
+    'select_price_range_product_index' => 'Selecciona el rango de precio',
     'less_than_50_product_index' => 'Menos de $50',
     'more_than_300_product_index' => 'Más de $300',
     'brand_product_index' => 'Marca',
@@ -43,19 +43,19 @@ return [
     'name_product_index' => 'Nombre',
     'example_play_5_product_index' => 'Ej.: Play 5',
     'close_product_index' => 'Cerrar',
-    'add_to_cart_product_index' => 'Añadir al carrito',
-    'success_message_product_index' => 'se añadió con éxito',
+    'add_to_cart_product_index' => 'Agregar al carrito',
+    'success_message_product_index' => 'fue agregado con éxito',
 
-    // Mensajes de producto
+    // Show product messages
     'category_product_show' => 'Categoría',
     'description_product_show' => 'Descripción',
     'keywords_product_show' => 'Palabras clave',
     'comment_product_show' => 'Comentario',
     'see_comments_product_show' => 'Ver Comentarios',
-    'add_to_cart_product_show' => 'Añadir al carrito',
+    'add_to_cart_product_show' => 'Agregar al Carrito',
     'success_product_review' => '¡Éxito!',
 
-    // Mensajes de edición de reseña
+    // Edit review messages
     'update_review_edit' => 'Actualizar Reseña',
     'description_review_edit' => 'Descripción',
     'rating_review_edit' => 'Calificación',
@@ -63,46 +63,46 @@ return [
     'success_update_review' => 'La reseña se ha editado con éxito',
     'success_review_edit' => '¡Éxito!',
 
-    // Mensajes de reseñas
+    // Show review messages
     'reviews_section_review_show' => '¡Mira las reseñas!',
     'comment_settings_review_show' => 'Configuración de comentarios',
     'delete_review_show' => 'Eliminar',
     'edit_review_show' => 'Editar',
     'reviews_empty_message_review_show' => 'Actualmente no hay reseñas.',
-    'hey_review_show' => '¡Hey!!!',
+    'hey_review_show' => '¡Hey!',
 
-    // Mensajes de creación de reseña
-    'add_comment_review_create' => 'Añadir Comentario',
+    // Create review messages
+    'add_comment_review_create' => 'Agregar Comentario',
     'description_review_create' => 'Descripción',
     'enter_description_review_create' => 'Ingresa una descripción',
     'rating_review_create' => 'Calificación',
     'send_review_create' => 'Enviar',
 
-    // Mensajes de carrito
+    // Index cart messages
     'shopping_cart' => 'Carrito de Compras',
     'price_cart_index' => 'Precio',
     'error_cart_index' => '¡Error!',
     'name_cart_index' => 'Producto',
     'quantity_cart_index' => 'Cantidad',
     'delete_cart_index' => 'Eliminar Artículo',
-    'remove_cart_index' => 'Remover Artículo',
+    'remove_cart_index' => 'Quitar Artículo',
     'actions_cart_index' => 'Acciones',
     'delete_button_cart_index' => 'Eliminar Carrito',
     'continue_button_cart_index' => 'Continuar',
-    'balance_error_cart_index' => 'Saldo insuficiente para completar la compra',
-    'stock_error_cart_index' => 'agotado',
-    'added_succesfully_product_index' => 'se añadió con éxito',
-    'empty_cart_index' => 'No tienes artículos en tu carrito, por favor añade productos para poder comprarlos',
+    'balance_error_cart_index' => 'Balance insuficiente para completar la compra',
+    'stock_error_cart_index' => 'fuera de stock',
+    'added_succesfully_product_index' => 'fue agregado con éxito',
+    'empty_cart_index' => 'No tienes ningún artículo en tu carrito, por favor agrega productos para poder comprarlos',
 
-    // Mensajes de pedido
-    'title_order_preorder' => 'Preorden',
+    // Order messages
+    'title_order_preorder' => 'Preordenar',
     'price_order_preorder' => 'Precio',
     'product_order_preorder' => 'Producto',
     'quantity_order_preorder' => 'Cantidad',
     'subtotal_order_preorder' => 'Subtotal',
     'total_order_preorder' => 'Total',
     'actions_order_preorder' => 'Acciones',
-    'go_back_button_order_preorder' => 'Volver',
+    'go_back_button_order_preorder' => 'Regresar',
     'confirm_order_button_order_preorder' => 'Confirmar Pedido',
     'table_order_order_index' => 'Pedido',
     'table_total_order_index' => 'Total',
@@ -122,7 +122,7 @@ return [
     'table_subtotal_order_show' => 'Subtotal',
     'table_total_order_show' => 'Total',
     'table_date_order_show' => 'Fecha',
-    'go_back_button_order_show' => 'Volver',
+    'go_back_button_order_show' => 'Regresar',
     'orders_empty_message' => 'Actualmente no hay pedidos.',
     'filter_orders_order_index' => 'Filtrar Pedidos',
     'date_before_filter_order_index' => 'Filtrar antes de',
@@ -130,24 +130,25 @@ return [
     'filter_order_index' => 'Filtrar',
     'close_order_index' => 'Cerrar',
 
-    // Mensajes de la barra de navegación
+    // Navbar messages
     'title_navbar_home' => 'PixelPlace',
     'register_button_navbar' => 'Registrarse',
     'login_button_navbar' => 'Iniciar Sesión',
-    'balance_icon_alt' => 'Saldo',
+    'balance_icon_alt' => 'Balance',
     'cart_icon_alt' => 'Carrito',
     'dropdown_avatar_alt' => 'Abrir menú de usuario',
     'settings_dropdown_navbar' => 'Configuración',
     'my_orders_dropdown_navbar' => 'Mis Pedidos',
-    'sign_out_dropdown_navbar' => 'Cerrar Sesión',
+    'sign_out_dropdown_navbar' => 'Cerrar sesión',
     'open_main_menu_icon_alt' => 'Abrir menú principal',
     'home_navbar' => 'Inicio',
     'products_navbar' => 'Productos',
     'challenges_navbar' => 'Desafíos',
     'contact_navbar' => 'Contacto',
     'marvel_navbar' => 'Marvel',
+    'plants_navbar' => 'Plantas',
 
-    // Mensajes de desafíos
+    // Index challenge messages
     'title_index_challenge' => 'Todos los Desafíos',
     'index_challenge_description' => 'Descripción: ',
     'index_challenge_reward' => 'Recompensa: ',
@@ -158,17 +159,17 @@ return [
     'index_challenge_goal' => 'Meta (cantidad de productos a comprar): ',
     'index_challenge_coins' => ' Monedas',
     'title_index_user_challenge' => 'Navega por los botones para ver tus desafíos',
-    'index_user_challenge_undone_challenge' => 'Sin Completar',
-    'index_user_challenge_done_challenge' => 'Completado',
+    'index_user_challenge_undone_challenge' => 'No Completados',
+    'index_user_challenge_done_challenge' => 'Completados',
     'index_user_challenge_in_progress_challenge' => 'En Progreso',
-    'index_user_challenge_undone_challenge_title' => 'Desafíos Sin Completar',
+    'index_user_challenge_undone_challenge_title' => 'Desafíos No Completados',
     'index_user_challenge_done_challenge_title' => 'Desafíos Completados',
     'index_user_challenge_in_progress_challenge_title' => 'Desafíos en Progreso',
-    'index_user_challenge_not_undone_challenge' => 'No tienes desafíos sin completar',
+    'index_user_challenge_not_undone_challenge' => 'No tienes desafíos no completados',
     'index_user_challenge_not_done_challenge' => 'No tienes desafíos completados',
     'index_user_challenge_not_in_progress_challenge' => 'No tienes desafíos en progreso',
     'index_user_challenge_description' => 'Descripción: ',
-    'index_user_challenge_reward' => 'Monedas de Recompensa: ',
+    'index_user_challenge_reward' => 'Recompensa en Monedas: ',
     'index_user_challenge_max_users' => 'Cantidad máxima de Usuarios: ',
     'index_user_challenge_current_users' => 'Usuarios Actuales: ',
     'index_user_challenge_expiration_date' => 'Fecha de Expiración: ',
@@ -179,8 +180,8 @@ return [
     'index_user_products_word' => ' Productos.',
     'success_challenge' => '¡Felicidades! Has completado el desafío: ',
 
-    // Configuración de usuario
-    'title_settings_index' => 'Configuración del Usuario',
+    // User settings index
+    'title_settings_index' => 'Configuración de Usuario',
     'button_orders_settings_index' => 'Mis Pedidos',
     'profile_picture_settings_index' => 'Foto de Perfil',
     'format_image_settings_index' => 'JPG, GIF o PNG.',
@@ -188,32 +189,52 @@ return [
     'form_email_settings_index' => 'Correo Electrónico',
     'form_button_settings_index' => 'Guardar Todo',
 
-    // Comando
-    'info_expired_challenges' => 'Desafíos vencidos comprobados con éxito.',
+    // Command
+    'info_expired_challenges' => 'Desafíos expirados revisados con éxito.',
 
-    // API de Marvel
+    // Marvel API
     'title_index_marvel' => 'Personajes de Marvel',
 
-    // Migas de pan
+    // Plant API
+    'title_index_plant' => 'Plantas',
+    'plant_price' => 'Precio: ',
+    'plant_category' => 'Categoría:',
+
+    // BreadCrumbs
     'home_breadcrumb' => 'Inicio',
     'settings_breadcrumb' => 'Configuración',
     'user_breadcrumb' => 'Usuario',
     'api_breadcrumb' => 'Api',
     'marvel_breadcrumb' => 'Marvel',
-
     'products_breadcrumb' => 'Productos',
+    'challenges_breadcrumb' => 'Desafíos',
+    'plant_breadcrumb' => 'Plantas',
     'reviews_breadcrumb' => 'Reseñas',
     'show_reviews_breadcrumb' => 'Mostrar',
     'edit_reviews_breadcrumb' => 'Editar',
     'create_reviews_breadcrumb' => 'Crear',
-
     'cart_breadcrumb' => 'Carrito',
-    'preorder_breadcrumb' => 'Preorden',
+    'preorder_breadcrumb' => 'Preordenar',
     'order_breadcrumb' => 'Pedido',
     'orders_breadcrumb' => 'Pedidos',
 
-    //  Idioma
+    //  Language
     'language' => 'Idioma',
     'english' => 'Inglés',
     'spanish' => 'Español',
+
+    // Auth
+    'sign_in_login' => 'Iniciar sesión en la plataforma',
+    'email_login' => 'Tu correo electrónico',
+    'password_login' => 'Tu contraseña',
+    'remember_login' => 'Recuérdame',
+    'forgot_password_login' => '¿Olvidaste tu contraseña?',
+    'title_login' => 'Inicia sesión en tu cuenta',
+    'not_register_login' => '¿No estás registrado? ',
+    'create_account_login' => 'Crear cuenta',
+    'create_account_register' => 'Crear una cuenta gratuita',
+    'name_register' => 'Tu nombre',
+    'confirm_pass_register' => 'Confirmar contraseña',
+    'already_account_register' => '¿Ya tienes una cuenta?',
+    'login_register' => 'Inicia sesión aquí',
 ];
